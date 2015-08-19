93f99c0d932d8931c012edd52370c1154a2f40b2
Max Strange <stranm1@spu.edu>
2015-08-18 19:23:04 -0700

    Wrote the test script for the hcsr04

--- HC_SR04_test/HC_SR04_test.ino
+++ HC_SR04_test/HC_SR04_test.ino
@@ -1,9 +1,47 @@
-void setup() {
-  // put your setup code here, to run once:
+const unsigned int TRIG_PIN = 13;
+const unsigned int ECHO_PIN = 12;
+const unsigned int LED = 11;
 
+void setup()
+{
+  Serial.begin (9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(LED, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  long duration, distance;
+  
+  /*
+  According to the datasheet, the module sends a pulse when the trigger receives
+  at least 10 microseconds of a high signal. So send a 2 us low, then 10 us high.
+  */ 
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  
+  duration = pulseIn(ECHO_PIN, HIGH);//Measures the amount of time the ECHO_PIN is HIGH - that time is the distance to the nearest object * 2
+  distance = (duration / 2) / 29.1;//speed of sound = 343 m/s, but the datasheet gives this as the formula to convert to cm
+  
+  if (distance < 4)  // This is where the LED On/Off happens
+    digitalWrite(LED, HIGH);
+  else 
+    digitalWrite(led,LOW);
+  
+  if (distance >= 200 || distance <= 0)
+  {
+    Serial.println("Out of range");
+  }
+  else
+  {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  
+  delay(500); //SUPER IMPORTANT! Have to make sure you don't trigger the module at too great a frequency - or you will
+  //get overlap between triggers and readings.
 }
